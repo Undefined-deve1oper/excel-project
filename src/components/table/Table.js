0e32e5c88b969ad7dda45191916dd8243832123c
Undefined-deve1oper <kononovkirill05@gmail.com>
2023-07-04 19:10:04 +0600

    Significant optimization

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,5 +1,5 @@
 import { ExcelComponent } from "@core/ExcelComponent";
-import { createTable } from "./table.template";
+import { createTable } from "@/components/table/table.template";
 import { $ } from "@core/Dom";
 
 export class Table extends ExcelComponent {
@@ -7,13 +7,12 @@ export class Table extends ExcelComponent {
 
     constructor($root) {
         super($root, {
-            name: "Table",
             listeners: ["mousedown"],
         });
     }
 
     toHTML() {
-        return createTable();
+        return createTable(30);
     }
 
     onMousedown(event) {
@@ -22,25 +21,42 @@ export class Table extends ExcelComponent {
             const $parent = $resizer.closest("[data-type=\"resizable\"]");
             const coords = $parent.getCoords();
             const type = $resizer.data.resize;
+            const sideProp = type === "col" ? "bottom" : "right";
+            let value;
 
-            const cells = this.$root.findAll(`[data-col="${$parent.data.col}"]`);
+            $resizer.css({
+                opacity: 1,
+                [sideProp]: "-5000px",
+            });
 
             document.onmousemove = (e) => {
                 if (type === "col") {
                     const delta = e.pageX - coords.right;
-                    const value = coords.width + delta;
-                    cells.forEach((el) => el.style.width = value + "px");
+                    value = coords.width + delta;
+                    $resizer.css({ right: -delta + "px" });
                 } else {
                     const delta = e.pageY - coords.bottom;
-                    const value = coords.height + delta;
-                    $parent.css({
-                        height: value + "px",
-                    });
+                    value = coords.height + delta;
+                    $resizer.css({ bottom: -delta + "px" });
                 }
             };
 
             document.onmouseup = () => {
                 document.onmousemove = null;
+                document.onmouseup = null;
+
+                if (type === "col") {
+                    this.$root.findAll(`[data-col="${$parent.data.col}"]`)
+                        .forEach((el) => el.style.width = value + "px");
+                } else {
+                    $parent.css({ height: value + "px" });
+                }
+
+                $resizer.css({
+                    opacity: 0,
+                    bottom: 0,
+                    right: 0,
+                });
             };
         }
     }
