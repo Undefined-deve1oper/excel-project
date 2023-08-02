39c28b443faf4c72213926f94edb6d264de26538
Undefined-deve1oper <kononovkirill05@gmail.com>
2023-08-02 19:23:43 +0600

    added the active state to the buttons in the toolbar

--- src/components/toolbar/toolbar.template.js
+++ src/components/toolbar/toolbar.template.js
@@ -17,34 +17,38 @@ function toButton(button) {
 export function createToolbar(state) {
     const buttons = [
         {
-            icon: "format_align_left",
-            active: false,
             value: { textAlign: "left" },
+            icon: "format_align_left",
+            active: state["textAlign"] === "left",
         },
         {
-            icon: "format_align_center",
-            active: false,
             value: { textAlign: "center" },
+            icon: "format_align_justify",
+            active: state["textAlign"] === "center",
         },
         {
-            icon: "format_align_right",
-            active: false,
             value: { textAlign: "right" },
+            icon: "format_align_right",
+            active: state["textAlign"] === "right",
         },
         {
+            value: { fontWeight: state["fontWeight"] === "bold" ? "normal" : "bold" },
             icon: "format_bold",
             active: state["fontWeight"] === "bold",
-            value: { fontWeight: state["fontWeight"] === "bold" ? "normal" : "bold" },
         },
         {
-            icon: "format_italic",
-            active: false,
-            value: { fontStyle: "italic" },
-        },
-        {
+            value: {
+                textDecoration: state["textDecoration"] === "underline"
+                    ? "none"
+                    : "underline",
+            },
             icon: "format_underlined",
-            active: false,
-            value: { textDecoration: "underline" },
+            active: state["textDecoration"] === "underline",
+        },
+        {
+            value: { fontStyle: state["fontStyle"] === "italic" ? "normal" : "italic" },
+            icon: "format_italic",
+            active: state["fontStyle"] === "italic",
         },
     ];
 
